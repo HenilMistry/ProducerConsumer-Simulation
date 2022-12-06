5bb49a108b5dce22a4d74111465adc164f7a15cb
DESKTOP-M4QI8VT\HENIL <henilmistry74496@gmail.com>
2022-12-06 12:11:13 +0530

    updates project settings and simulated the unbounded buffer problem with new values

--- ProducerConsumer/src/UnboundedBuffer/Main.java
+++ ProducerConsumer/src/UnboundedBuffer/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Integer unboundedBuffer = 0;
 
-        Thread p = new Thread(new Producer(unboundedBuffer, 5));
-        Thread p1 = new Thread(new Producer(unboundedBuffer, 5, 2000));
-        Thread c = new Thread(new Consumer(unboundedBuffer, 11, 5000));
+        Thread p = new Thread(new Producer(unboundedBuffer, 2));
+        Thread p1 = new Thread(new Producer(unboundedBuffer, 2, 2000));
+        Thread c = new Thread(new Consumer(unboundedBuffer, 5, 4000));
 
         p.setName("Farmer");
         p1.setName("Farmer 1");
